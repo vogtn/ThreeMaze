f58d9251b43575c6c3ecfe394c45936eb4e0a975
vogtn <nicolasjvogt@gmail.com>
2017-02-12 09:27:37 -0800

    adding scene objects

--- main.js
+++ main.js
@@ -88,3 +88,49 @@ function animate() {
   }
   render();
 }
+
+
+
+function setupScene(){
+  var units = mapW;
+
+  var floor = new t.Mesh(
+    new t.CubeGeometry(units * unitsize, 10, units *  unitsize),
+    new t.MeshLambertMaterial({color: 0xEDCBA0})
+  );
+  scene.add(floor);
+
+  var cube = new t.CubeGeometry(unitsize, wallheight, unitsize);
+  var materials = [
+    new t.MeshLambertMaterial({map: t.ImageUtils.loadTexture('images/wall-1.jpg')}),
+    new t.MeshLambertMaterial({map: t.ImageUtils.loadTexture('images/wall-2.jpg')}),
+  ];
+
+  for(var i=0; i< mapW; i++){
+    for(var j=0; m=map[i].length; j<m; j++){
+      if(map[i][j]){
+        var wall = new t.Mesh(cube, materials[map[i][j]-1]);
+        wall.position.x = (i-units/2) * unitsize;
+        wall.position.y = wallheight/2;
+        wall.position.z = (j-units/2) * unitsize;
+        scene.add(wall);
+      }
+    }
+  }
+
+  //speed cube
+  speedcube = new t.Mesh(
+    new t.CubeGeometry(30, 30, 30),
+    new t.MeshBasicMaterial({map: t.ImageUtils.loadTexture('images/speed.png')})
+  );
+  speedcube.position.set(-unitsize-15, 35, -unitsize-15);
+  scene.add(speedcube);
+
+  //lights
+  var directionalLight1 = new t.DirectionalLight( 0xF7EFBE, 0.7);
+  directionalLight1.position.set(0.5, 1, 0.5);
+  scene.add(directionalLight1);
+  var directionalLight2 = new t.DirectionalLight( 0xF7EFBE, 0.5);
+  directionalLight2.position.set(-0.5, -1, -0.5);
+  scene.add(directionalLight2);
+}
