523cae22418828ef8ee6d5e8051e4f73dae105ac
vogtn <nicolasjvogt@gmail.com>
2017-02-11 23:26:42 -0800

    render helpers

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ mouse = {x:0, y:0};
 var runBoost, lastRunBoost = 0;
 
 // map is x index (1-9), y index (0-9)
+// 0 is character location 1 is wall, 2 is final area
 
 var map = [
            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
@@ -40,3 +41,50 @@ $(document).ready(function(){
     animate();
   });
 });
+
+//initialize game
+function init() {
+  clock = new t.Clock(); //timer for rendering smooth animation
+  projector = new t.Projector(); //2d ray
+  scene = new t.Scene(); //world
+  scene.fog = new t.FogExp2(0xD6F1FF, 0.0005); //adds fog
+
+  //camera
+  cam = new t.PerspectiveCamera(60, aspect, 1, 10000); // FOV,
+  cam.position.y = UNITSIZE * 0.2;
+  scene.add(cam);
+
+  //camera controls
+  controls = new t.FirstPersonControls(cam);
+  controls.movementSpeed = movespeed;
+  controls.lookSpeed = lookspeed;
+  controls.lookVertial = false;
+  controls.noFly = true;
+
+  //world objects
+  setupScene();
+
+  renderer = new t.WebGlRenderer();
+  renderer.setSize(width, height);
+
+  renderer.domElement.style.backgroundColor = '#D6F1FF';
+  document.body.appendChild(renderer.domElement);
+
+  //Track Mouse position
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+  //display
+  $('body').append('<canvas id="radar" widht="200" height="200"></canvas>');
+  $('body').append('<div id="hud">Score: <span id="score">0</span></div>');
+
+  //win condition
+  $('body').append('<div id="win"></div>');
+  $('#win').css({width: width, height: height});
+}
+
+function animate() {
+  if(runAnim){
+    requestAnimationFrame(animate);
+  }
+  render();
+}
